592a74d526a30941861ea8c4a491ff8bf4603413
Tomas Aranda <tomasdeveloperfrontend@gmail.com>
2022-02-01 04:28:23 -0300

    v1.0.0

--- styles/app.js
+++ styles/app.js
@@ -1,13 +1,14 @@
+
+// function img slider cups smalls > cups bigs
 function imgSlider(cambio) {
     document.querySelector("body > div > div.flex > div > img").src = cambio;
-    let img =  document.querySelector("body > div > div.flex > div > img")
-    img.style.height = "38rem"
-}
+    let img =  document.querySelector("body > div > div.flex > div > img");
+    img.style.height = "38rem";
+};
 
-
+// function change circle color 
 function changeCircleColor(color) {
-    const circle = document.querySelector(".circle")
+    const circle = document.querySelector(".circle");
     circle.style.backgroundColor = color;
-    
-}
+};
 
